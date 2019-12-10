bafd9902c2dcc76b0476230ca6b3a12db3169149
Ashley Dunham <xenocide21@gmail.com>
2019-12-10 15:08:34 -0800

    reducer complete, all functionality tested, MVP met.

--- src/src/component/TodoForm.js
+++ src/src/component/TodoForm.js
@@ -1,5 +1,5 @@
 import React, { useState, useReducer } from 'react'
-import { todoReducer, initialState } from '../reducers/todoReducer'
+import { todoReducer, initialState } from '../reducer/todoReducer'
 
 const TodoForm = () => {
     const [newItem, setNewItem] = useState('')
@@ -13,7 +13,7 @@ const TodoForm = () => {
         <div className='todo-form'>
             {state.todos.map(todo => {
                 return <div className='todo-item'>
-                    <p className={todo.complete ? 'todo-complete' : '')}>{todo.item}</p>
+                    <p className={todo.complete ? 'todo-complete' : ''}>{todo.item}</p>
                     <button onClick={
                         () => { dispatch({type: 'TOGGLE_COMPLETE', payload: todo.id})}
                     }>Complete</button>
@@ -21,7 +21,7 @@ const TodoForm = () => {
             })}
 
             <input className='todo-input' type='text' name='newItem' value={newItem} onChange={handleChange}/>
-            <button onClick={()=>{dispatch({type: 'ADD_TODO', payloadL newItem})}}>Add New Todo</button>
+            <button onClick={()=>{dispatch({type: 'ADD_TODO', payload: newItem})}}>Add New Todo</button>
             <button onClick={()=>{dispatch({type: 'CLEAR_COMPLETE'})}}>Clear Completed Todo</button>
         </div>
     )
